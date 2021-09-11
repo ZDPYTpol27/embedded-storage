1f0b2c11535d0a1d67c5378b1abc2a04810edc9c
Vitaliy <vetal444ka@gmail.com>
2021-09-12 01:50:29 +0200

    The 5-th edition

--- newApp_2/newApp/ell_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Resistors, Chip, Capacitor, Diode, Transistor
-
-admin.site.register(Resistors)
-admin.site.register(Chip)
-admin.site.register(Capacitor)
-admin.site.register(Diode)
-admin.site.register(Transistor)
-# Register your models here.
--- newApp_2/newApp/ell_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EllAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ell_app'
